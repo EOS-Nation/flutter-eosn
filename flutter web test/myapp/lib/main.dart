214b36785d9831a0cf274ecca75fd470d0b26465
paco <picc2711@usherbrooke.ca>
2020-07-14 13:09:00 -0400

    firebase flag for local test

--- flutter web test/myapp/lib/main.dart
+++ flutter web test/myapp/lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/models/user.dart';
+import 'package:myapp/pages/home/home.dart';
 import 'package:myapp/pages/wrapper.dart';
 import 'package:myapp/services/auth.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+final bool useFirebaseLogin = false;
 
-class MyApp extends StatelessWidget {
+void main() =>
+    runApp(useFirebaseLogin ? MyAppWithFirebase : MyAppWithoutFirebase());
+
+class MyAppWithoutFirebase extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF3a3a3a),
+        primaryColorDark: Color(0xFF152026),
+        buttonColor: Color(0xFFd66c44),
+      ),
+    );
+  }
+}
+
+class MyAppWithFirebase extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
